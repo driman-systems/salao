8b1973d8e71b6d742afa7f9ddbc451d647cdbd18
Driman Systems <anderstrak@gmail.com>
2023-04-23 17:27:10 -0300

    img

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const router = useRouter();
   return (
     <header className="flex border-b-2 border-gray-100 shadow-sm shadow-gray-100 bg-violet-50">
       <div className="flex w-full max-w-7xl m-auto py-3 px-3 xl:px-0 justify-between items-center">
-            <Image onClick={()=> router.push("/")} className="cursor-pointer" src="/logo-amanda.png" alt="Amanda Moura Nail Design" width={200} height={58} />
+            <Image onClick={()=> router.push("/")} className="cursor-pointer max-w-[150px] h-auto" src="/logo-amanda.png" alt="Amanda Moura Nail Design" width={200} height={58} />
             <nav className="space-x-6 hidden lg:inline">
               <Link className="text-gray-700" href="/sobre">Sobre</Link>
               <Link className="text-gray-700" href="/servicos">Servicos</Link>

6774e7a154adf8c9ab1ace7e0f21908048d0fa77
Driman Systems <anderstrak@gmail.com>
2023-04-24 07:23:56 -0300

    logo branco

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,13 +10,13 @@ const Header = () => {
 const router = useRouter();
 
   return (
-    <header className="flex border-b-2 border-gray-100 shadow-sm shadow-gray-100 bg-[#f0d4dd]">
+    <header className="flex border-b-2 border-gray-100 shadow-sm shadow-gray-100 bg-[#A82D54]">
       <div className="flex w-full max-w-7xl m-auto py-3 px-3 xl:px-0 justify-between items-center">
-            <Image onClick={()=> router.push("/")} className="cursor-pointer max-w-[150px] h-auto" src="/logo-amanda.png" alt="Amanda Moura Nail Design" width={200} height={58} />
+            <Image onClick={()=> router.push("/")} className="cursor-pointer max-w-[150px] h-auto" src="/logo-amanda.png" alt="Amanda Moura Nail Design" width={180} height={50} />
             <nav className="space-x-6 hidden lg:inline">
-              <Link className="text-gray-700" href="/sobre">Sobre</Link>
-              <Link className="text-gray-700" href="/servicos">Servicos</Link>
-              <Link className="text-gray-700 items-center" href="/login">Entrar <FaSignOutAlt className="inline" /></Link>
+              <Link className="text-white" href="/sobre">Sobre</Link>
+              <Link className="text-white" href="/servicos">Servicos</Link>
+              <Link className="text-white items-center" href="/login">Entrar <FaSignOutAlt className="inline" /></Link>
             </nav>
             <RiMenuUnfoldLine className="lg:hidden text-4xl me-3 text-[#A82D54]" />
       </div>

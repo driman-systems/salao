939b824ed49305f9964d89fe16a342d028770a56
Driman Systems <anderstrak@gmail.com>
2023-04-26 06:41:48 -0300

    button login

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -8,12 +8,12 @@ const Login = () => {
     <div>
       <section class="bg-gray-50">
         <div class="flex flex-col h-screen items-center justify-center px-6 py-8 mx-auto lg:py-0">
-            <a href="#" class="flex items-center mb-6">
+            <a href="/" class="flex items-center mb-6">
                 <Image class="mr-1" src="/logo-amanda-dark.svg" alt="logo" width={250} height={72} />  
             </a>
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                         Entrar
                     </h1>
                     <form class="space-y-4 md:space-y-6" action="#">
@@ -42,7 +42,7 @@ const Login = () => {
                             </div>
                             <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Esqueceu a senha?</a>
                         </div>
-                        <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                        <button type="submit" class="w-full text-white bg-[#A82D54] hover:brightness-105 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-md text-sm px-5 py-2.5 text-center">Entrar</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                             Não tem conta? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Cadastrar</a>
                         </p>
